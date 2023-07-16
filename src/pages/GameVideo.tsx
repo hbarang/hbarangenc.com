c9d63b19575a8248ce29b13f8bf1da3537267296
emirakbay <akbayemir@gmail.com>
2023-07-16 21:10:47 +0300

    Update GameVideo.tsx

--- src/pages/GameVideo.tsx
+++ src/pages/GameVideo.tsx
@@ -1,12 +1,19 @@
-import { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import games from "./api/gameInfo";
+import { useState, useEffect } from "react";
 
 export default function GameVideo({ index }: { index: number }) {
+  const [hasWindow, setHasWindow] = useState(false);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setHasWindow(true);
+    }
+  }, []);
+
   return (
     <>
       <div className="video">
-        {<ReactPlayer url={games[index]!.videoLink} />}
+        {hasWindow && <ReactPlayer url={games[index]!.videoLink} />}
       </div>
     </>
   );
